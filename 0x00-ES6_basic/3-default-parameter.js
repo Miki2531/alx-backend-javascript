50d284b12b58b29dbe68f1c41ac3fa9d8fa4d0a8
Miki2531 <mikiyasshemelis46@gmail.com>
2023-06-26 21:40:50 +0300

    object

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,12 +1,7 @@
 export default function getSumOfHoods(
-  initialNumber = 1,
-  expansion1989 = 1,
-  expansion2019 = 1
+  initialNumber,
+  expansion1989 = 89,
+  expansion2019 = 19
 ) {
-  if (expansion1989 && expansion2019 === undefined) {
-    expansion1989 = 89;
-    expansion2019 = 19;
-  }
-
   return initialNumber + expansion1989 + expansion2019;
 }

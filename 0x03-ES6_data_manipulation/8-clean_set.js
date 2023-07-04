1011ab09a7ebcc2e8b876c29bd015252dd9f4f98
Miki2531 <mikiyasshemelis46@gmail.com>
2023-07-04 23:25:24 +0300

    'done'

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,29 @@
+/**
+ * Joins a set of strings with a dash after stripping the strings of
+ * a leading sub string.
+ * @param {Set<String>} set - A collection of strings.
+ * @param {String} startString - The string to strip from the beginning
+ * of each item in the set.
+ * @returns {String}
+ */
 export default function cleanSet(set, startString) {
-  if (startString === undefined || startString.length === 0) {
+  const parts = [];
+  if (
+    !set
+    || !startString
+    || !(set instanceof Set)
+    || typeof startString !== 'string'
+  ) {
     return '';
   }
-  return [...set]
-    .filter((str) => (str !== undefined ? str.startswith(startString) : ''))
-    .map((str) => (str !== undefined ? str.slice(startString.length) : ''))
-    .join('-');
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
 }
